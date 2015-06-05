3dc4f84727ae1a82929b476d4888c3694638c7a3
Jeremy Leggat <jleggat@asu.edu>
2015-06-05 16:36:38 -0700

    Use javascript to move by line with date line in posts
    update of javascript to move the "byline" spans to footer.

--- js/move-by-line.js
+++ js/move-by-line.js
@@ -6,6 +6,8 @@
 
 ( function( $ ) {
 	$('#main article').each(function(index, value) { 
+		var byLine = $( value ).find('span.byline').detach();
+		$( value ).find('.entry-footer').prepend( byLine );
 		var postedOn = $( value ).find('span.posted-on').detach();
 		$( value ).find('.entry-footer').prepend( postedOn );
 	})
